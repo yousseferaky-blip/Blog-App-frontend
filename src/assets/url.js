155867319e2cc55650be20824eb73a12d61d04fc
yousseferaky-blip <yeraky07@gmail.com>
2024-01-17 00:32:34 +0300

    update BASE_URL

--- src/assets/url.js
+++ src/assets/url.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:5000' ;
-export const BASE_URL = 'https://blog-app-backend-seven-dusky.vercel.app' ;
+export const BASE_URL = 'https://blog-app-backend-jp2w.onrender.com' ;
